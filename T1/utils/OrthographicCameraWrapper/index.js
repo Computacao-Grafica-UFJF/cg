97ce7e25d7b81618dc443b37da5a2cd1eec46d29
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2023-08-30 12:08:53 -0300

    fix: Corrigindo zoom da camera

--- T1/utils/OrthographicCameraWrapper/index.js
+++ T1/utils/OrthographicCameraWrapper/index.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 class OrthographicCameraWrapper extends THREE.OrthographicCamera {
     constructor() {
         const aspect = window.innerWidth / window.innerHeight;
-        const width = 50;
+        const width = 60;
         const height = width / aspect;
 
         super(width / -2, width / 2, height / 2, height / -2, -1000, 1000);
