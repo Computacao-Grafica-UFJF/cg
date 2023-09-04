47ed4ae6b04f80026f9c8a197b27756936a50dde
Charles Lelis Braga <charles.braga.jf.cb@gmail.com>
2023-09-04 11:33:34 -0300

    feat: getRandom angle to down

--- T1/sprites/MiniBall/index.js
+++ T1/sprites/MiniBall/index.js
@@ -12,56 +12,61 @@ export class MiniBall extends THREE.Mesh {
         this.translateZ(z);
 
         this.speed = 0.3;
-        this.angle = -Math.PI / 2;
+        this.angle = this.getRandomAngleToDown();
 
         this.rotateZ(this.angle);
     }
 
-    radToDeg = (rad) => {
-        return rad * (180 / Math.PI);
+    getRandomAngleToDown = () => {
+        const min = 10;
+        const max = 170;
+
+        const randomAngle = Math.random() * (max - min) + min + 180;
+
+        return THREE.MathUtils.degToRad(randomAngle);
+    };
+
+    collisionWithHitter = (hitter) => {
+        const ballBoundingBox = new THREE.Box3().setFromObject(this);
+        const hitterBoundingBox = new THREE.Box3().setFromObject(hitter);
+
+        if (ballBoundingBox.intersectsBox(hitterBoundingBox)) {
+            const relativeX = this.position.x - hitter.position.x;
+            const angle = hitter.getKickBallAngle(relativeX);
+            this.angle = angle;
+            this.rotation.set(0, 0, angle);
+        }
+    };
+
+    collisionWithWalls = (walls) => {
+        const ballBoundingBox = new THREE.Box3().setFromObject(this);
+
+        walls.forEach((wall) => {
+            const wallBoundingBox = new THREE.Box3().setFromObject(wall);
+
+            if (ballBoundingBox.intersectsBox(wallBoundingBox)) {
+                this.angle = wall.type === "horizontal" ? -this.angle : Math.PI - this.angle;
+                this.rotation.set(0, 0, this.angle);
+            }
+        });
+    };
+
+    collisionWithDeathZones = (deathZones) => {
+        const ballBoundingBox = new THREE.Box3().setFromObject(this);
+
+        deathZones.forEach((deathZone) => {
+            const wallBoundingBox = new THREE.Box3().setFromObject(deathZone);
+
+            if (ballBoundingBox.intersectsBox(wallBoundingBox)) {
+                this.speed = 0;
+            }
+        });
     };
 
     update(hitter, walls, blocks, deathZones) {
-        const collisionWithHitter = () => {
-            const ballBoundingBox = new THREE.Box3().setFromObject(this);
-            const hitterBoundingBox = new THREE.Box3().setFromObject(hitter);
-
-            if (ballBoundingBox.intersectsBox(hitterBoundingBox)) {
-                const relativeX = this.position.x - hitter.position.x;
-                const angle = hitter.getKickBallAngle(relativeX);
-                this.angle = angle;
-                this.rotation.set(0, 0, angle);
-            }
-        };
-
-        const collisionWithWalls = () => {
-            const ballBoundingBox = new THREE.Box3().setFromObject(this);
-
-            walls.forEach((wall) => {
-                const wallBoundingBox = new THREE.Box3().setFromObject(wall);
-
-                if (ballBoundingBox.intersectsBox(wallBoundingBox)) {
-                    this.angle = wall.type === "horizontal" ? -this.angle : Math.PI - this.angle;
-                    this.rotation.set(0, 0, this.angle);
-                }
-            });
-        };
-
-        const collisionWithDeathZones = () => {
-            const ballBoundingBox = new THREE.Box3().setFromObject(this);
-
-            deathZones.forEach((deathZone) => {
-                const wallBoundingBox = new THREE.Box3().setFromObject(deathZone);
-
-                if (ballBoundingBox.intersectsBox(wallBoundingBox)) {
-                    this.speed = 0;
-                }
-            });
-        };
-
-        collisionWithHitter();
-        collisionWithWalls();
-        collisionWithDeathZones();
+        this.collisionWithHitter(hitter);
+        this.collisionWithWalls(walls);
+        this.collisionWithDeathZones(deathZones);
         this.translateX(this.speed);
     }
 }
