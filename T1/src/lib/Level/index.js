a846176c270bd55572139933e974ee0074d125d7
CharlesLB <charles.braga.jf.cb@gmail.com>
2023-10-03 22:28:09 -0300

    feat: modularize Level

--- T1/src/lib/Level/index.js
+++ T1/src/lib/Level/index.js
@@ -3,6 +3,12 @@ import { onWindowResize } from "../../../../libs/util/util.js";
 import KeyboardCommands from "../../utils/KeyboardCommands/index.js";
 import Pause from "../../sprites/Pause/index.js";
 import Game from "../Game/index.js";
+import AuxiliarPlatform from "../../sprites/AuxiliarPlatform/index.js";
+import Hitter from "../../sprites/Hitter/index.js";
+import Platform from "../../sprites/Platform/index.js";
+import MiniBall from "../../sprites/MiniBall/index.js";
+import Wall from "../../sprites/Wall/index.js";
+import gameConfig from "../../config/Game.js";
 
 class Level {
     constructor() {
@@ -32,6 +38,77 @@ class Level {
             this.raycaster.onMouseDown(event, this);
         });
     };
+
+    start() {
+        this.gamePlatform = this.buildGamePlatform();
+        this.platform = this.buildPlatform();
+        this.hitter = this.buildHitter();
+        this.playablePlatform = this.buildPlayablePlatform();
+        this.miniBall = this.buildMiniBall();
+        this.walls = [...this.buildWalls()];
+        this.blocks = [...this.buildBlocks()];
+
+        Game.scene.add(...this.getElements());
+    }
+
+    buildGamePlatform() {
+        const gamePlatform = new Platform(innerWidth, innerHeight, "#202030");
+        return gamePlatform;
+    }
+
+    buildPlatform() {
+        const platform = new Platform(gameConfig.width, 2 * gameConfig.width, "#47454E");
+        return platform;
+    }
+
+    buildPlayablePlatform() {
+        const playablePlatform = new AuxiliarPlatform(15 - this.hitter.geometry.parameters.width, 30);
+        return playablePlatform;
+    }
+
+    buildHitter() {
+        const hitter = new Hitter(0, -13, 0, "rgb(255,255,255)");
+        return hitter;
+    }
+
+    buildMiniBall() {
+        const positionStartX = 0.8;
+        const positionStartY = -12;
+
+        const miniBall = new MiniBall(positionStartX, positionStartY, 0, "#fff");
+        return miniBall;
+    }
+
+    buildWalls() {
+        const wallTop = new Wall(0, gameConfig.width + 0.5, 0, gameConfig.width + 2, 1, "horizontal");
+        const wallBottom = new Wall(0, -gameConfig.width - 0.5, 0, gameConfig.width + 2, 1, "horizontal");
+        const wallLeft = new Wall(-gameConfig.width / 2 - 0.5, 0, 0, 1, 2 * gameConfig.width, "vertical");
+        const wallRight = new Wall(gameConfig.width / 2 + 0.5, 0, 0, 1, 2 * gameConfig.width, "vertical");
+
+        return [wallTop, wallBottom, wallLeft, wallRight];
+    }
+
+    getElements() {
+        return [this.gamePlatform, this.platform, this.hitter, this.playablePlatform, this.miniBall, ...this.walls, ...this.blocks];
+    }
+
+    finishedLevel() {
+        return this.blocks.length === 0;
+    }
+
+    destroyBlock(block) {
+        this.blocks = this.blocks.filter((b) => b !== block);
+        Game.scene.remove(block);
+
+        this.finishedLevel() && setTimeout(() => this.finish(), 10);
+    }
+
+    moveMiniBall() {
+        const collisionWalls = [this.walls[0], this.walls[2], this.walls[3]];
+        const deathZones = [this.walls[1]];
+
+        this.miniBall.update(this.hitter, collisionWalls, this.blocks, deathZones, this.destroyBlock.bind(this), this.death.bind(this));
+    }
 
     restart() {
         Game.scene.remove(...this.getElements());
--- T1/src/scenes/Level1/index.js
+++ T1/src/scenes/Level1/index.js
@@ -1,51 +1,13 @@
-import Hitter from "../../sprites/Hitter/index.js";
-import Platform from "../../sprites/Platform/index.js";
-import MiniBall from "../../sprites/MiniBall/index.js";
-import Wall from "../../sprites/Wall/index.js";
-import gameConfig from "../../config/Game.js";
-
 import BlocksBuilder from "../../utils/BlocksBuilder/index.js";
-import AuxiliarPlatform from "../../sprites/AuxiliarPlatform/index.js";
+
 import Level from "../../lib/Level/index.js";
 import Game from "../../lib/Game/index.js";
 
 class Level1 extends Level {
-    constructor(camera, renderer, scene) {
-        super(camera, renderer, scene);
+    constructor() {
+        super();
 
         this.start();
-    }
-
-    start() {
-        this.gamePlatform = this.buildGamePlatform();
-        this.platform = this.buildPlatform();
-        this.hitter = this.buildHitter();
-        this.playablePlatform = this.buildPlayablePlatform();
-        this.miniBall = this.buildMiniBall();
-        this.walls = [...this.buildWalls()];
-        this.blocks = [...this.buildBlocks()];
-
-        Game.scene.add(...this.getElements());
-    }
-
-    buildGamePlatform() {
-        const gamePlatform = new Platform(innerWidth, innerHeight, "#202030");
-        return gamePlatform;
-    }
-
-    buildPlatform() {
-        const platform = new Platform(gameConfig.width, 2 * gameConfig.width, "#47454E");
-        return platform;
-    }
-
-    buildPlayablePlatform() {
-        const playablePlatform = new AuxiliarPlatform(15 - this.hitter.geometry.parameters.width, 30);
-        return playablePlatform;
-    }
-
-    buildHitter() {
-        const hitter = new Hitter(0, -13, 0, "rgb(255,255,255)");
-        return hitter;
     }
 
     buildBlocks() {
@@ -62,48 +24,9 @@ class Level1 extends Level {
 
         return blocks;
     }
-
-    buildMiniBall() {
-        const positionStartX = 0.8;
-        const positionStartY = -12;
-
-        const miniBall = new MiniBall(positionStartX, positionStartY, 0, "#fff");
-        return miniBall;
-    }
-
-    buildWalls() {
-        const wallTop = new Wall(0, gameConfig.width + 0.5, 0, gameConfig.width + 2, 1, "horizontal");
-        const wallBottom = new Wall(0, -gameConfig.width - 0.5, 0, gameConfig.width + 2, 1, "horizontal");
-        const wallLeft = new Wall(-gameConfig.width / 2 - 0.5, 0, 0, 1, 2 * gameConfig.width, "vertical");
-        const wallRight = new Wall(gameConfig.width / 2 + 0.5, 0, 0, 1, 2 * gameConfig.width, "vertical");
-
-        return [wallTop, wallBottom, wallLeft, wallRight];
-    }
-
-    getElements() {
-        return [this.gamePlatform, this.platform, this.hitter, this.playablePlatform, this.miniBall, ...this.walls, ...this.blocks];
-    }
-
-    finishedGame() {
-        return this.blocks.length === 0;
-    }
-
-    destroyBlock(block) {
-        this.blocks = this.blocks.filter((b) => b !== block);
-        Game.scene.remove(block);
-
-        this.finishedGame() && setTimeout(() => this.finish(), 10);
-    }
-
-    moveMiniBall() {
-        const collisionWalls = [this.walls[0], this.walls[2], this.walls[3]];
-        const deathZones = [this.walls[1]];
-
-        this.miniBall.update(this.hitter, collisionWalls, this.blocks, deathZones, this.destroyBlock.bind(this), this.death.bind(this));
-    }
 }
 
-const level = new Level1(Game.camera, Game.renderer, Game.scene);
+const level = new Level1();
 
 const render = () => {
     level.keyboardUpdate(level);
