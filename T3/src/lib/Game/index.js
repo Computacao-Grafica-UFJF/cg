05bdf7df6dd45f1282a83b6287ed59a6aa13d9cb
CharlesLB <charles.braga.jf.cb@gmail.com>
2023-11-20 17:53:06 -0300

    feat: camera position

--- T3/src/lib/Game/index.js
+++ T3/src/lib/Game/index.js
@@ -35,9 +35,9 @@ class Game {
     }
 
     static fixCameraPosition() {
-        const controls = new OrbitControls(this.camera, this.renderer.domElement);
-        controls.enabled = false;
-        controls.enableZoom = false;
+        // const controls = new OrbitControls(this.camera, this.renderer.domElement);
+        // controls.enabled = false;
+        // controls.enableZoom = false;
     }
 
     static pause() {
--- T3/src/utils/PerspectiveCameraWrapper/index.js
+++ T3/src/utils/PerspectiveCameraWrapper/index.js
@@ -4,13 +4,13 @@ class PerspectiveCameraWrapper extends THREE.PerspectiveCamera {
     constructor() {
         super();
 
-        const fov = 73;
+        const fov = 43.3;
         const aspect = window.innerWidth / window.innerHeight;
         const near = 0.1;
         const far = 1000;
 
-        const position = new THREE.Vector3(0, 0, 22);
-        const lookAt = new THREE.Vector3(0, 0, 0);
+        const position = new THREE.Vector3(0, -17, 30);
+        const lookAt = new THREE.Vector3(0, -2, 0);
 
         this.position.copy(position);
         this.lookAt(lookAt);
